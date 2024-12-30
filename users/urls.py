ddadb32770aad96a866ed5fdeb3591e1a181b0b2
Yuval-Betito <yuval36811@gmail.com>
2024-12-31 01:48:08 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,9 @@
-# users/urls.py
-
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
+from .views import register, user_login, home, create_customer
 
 urlpatterns = [
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
-    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
-    path('change-password/', views.change_password_view, name='change_password'),  # If using a custom view
-    path('home/', views.home_view, name='home'),  # If needed within users
-    # Add other user-related paths here
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('home/', home, name='home'),
+    path('create_customer/', create_customer, name='create_customer'),
 ]
