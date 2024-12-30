69fa13dcf5db8b178cc5389a837cf1652bb0cad6
Yuval-Betito <yuval36811@gmail.com>
2024-12-31 00:36:52 +0200

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import register, user_login, home, create_customer
+from django.contrib.auth.views import LogoutView
+from . import views
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
-    path('home/', home, name='home'),
-    path('create_customer/', create_customer, name='create_customer'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home_view, name='home'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('change-password/', views.change_password_view, name='change_password'),
+    # Add other paths as needed
 ]
